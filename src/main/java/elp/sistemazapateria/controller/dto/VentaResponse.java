1a6b8b06150995c49691eda51c4718c592044f2d
Deyker <gamboadeyker@gmail.com>
2025-06-19 18:09:55 -0500

    arreglos en VentaRequest  y response

--- src/main/java/elp/sistemazapateria/controller/dto/VentaResponse.java
+++ src/main/java/elp/sistemazapateria/controller/dto/VentaResponse.java
@@ -1,14 +1,18 @@
 package elp.sistemazapateria.controller.dto;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import lombok.Data;
 
 @Data
 public class VentaResponse {
-	 private Integer identificador;
-	 private String idCliente;
-		private LocalDateTime fecha;
-		private String metodoPago;
-		private Cliente cliente;
+	
+	private Integer idVenta;
+	private Long idCliente;
+	private String nombreCliente; //para mostrar el nombre del cliente directamente
+	private LocalDateTime fecha;
+	private String metodoPago;
+	private String estado;
+	private List<VentaDetalleResponse> detalles; // Lista de detalles de la venta
 }
