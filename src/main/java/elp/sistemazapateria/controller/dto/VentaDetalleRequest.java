3b91f20bb11ea63d8b3bbac0b630444bd50e429d
daniel <leandroaybarccristian@.com>
2025-06-19 20:37:33 -0500

    subiendo clases  VentaDetalle

--- src/main/java/elp/sistemazapateria/controller/dto/VentaDetalleRequest.java
+++ src/main/java/elp/sistemazapateria/controller/dto/VentaDetalleRequest.java
@@ -1,5 +1,14 @@
 package elp.sistemazapateria.controller.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+//DTO para los detalles de una venta (usado dentro de VentaRequest)
+@Getter
+@Setter
 public class VentaDetalleRequest {
-
+	   private Integer idVenta;   // Nuevo: ID de la venta a la que pertenece el detalle
+	    private Integer idCalzado; // ID del calzado
+	    private Integer cantidad;
+	    private Double total;
+	    private Boolean estado;
 }
--- src/main/java/elp/sistemazapateria/controller/dto/VentaDetalleResponse.java
+++ src/main/java/elp/sistemazapateria/controller/dto/VentaDetalleResponse.java
@@ -1,5 +1,16 @@
 package elp.sistemazapateria.controller.dto;
 
-public class VentaDetalleResponse {
+import lombok.Data;
 
+//DTO para los detalles de una venta (usado dentro de VentaResponse)
+@Data
+public class VentaDetalleResponse {
+// Los IDs de la clave compuesta se pueden incluir si son relevantes para el frontend
+private Integer idVenta; // El ID de la venta a la que pertenece este detalle
+private Integer idCalzado; // El ID del calzado en este detalle
+private String marcaCalzado; // Opcional: para mostrar la marca del calzado
+private String descripcionModeloCalzado; // Opcional: para mostrar la descripción del modelo del calzado
+private Integer cantidad;
+private Double total;
+private Boolean estado;
 }
